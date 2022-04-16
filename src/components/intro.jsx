ad7d0579144fd441ef936e2da2424ff4f160601c
OKitel <kitelolga@gmail.com>
2022-04-17 00:11:14 +0200

    feat: correct intro section

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -14,15 +14,16 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Hello, I am Olga Kitel</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
                   <Typed
                     strings={[
-                      "Front End Developer",
-                      "Back End Developer",
-                      "Software Engineer"
+                      "Front-end Developer",
+                      "Junior Web Developer",
+                      "React Developer",
+                      "Self-taught programmer",
                     ]}
                     typeSpeed={80}
                     backDelay={1100}
@@ -37,7 +38,7 @@ class Intro extends React.Component {
                   href="#work"
                   role="button"
                 >
-                  View My Work
+                  View My Projects
                 </a>
               </p>
             </div>
